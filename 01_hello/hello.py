d0e90895ba9cd0910b1be27b9f26e8646f8d7aba
Justin Byers <justin.byers64@gmail.com>
2021-01-10 15:37:59 +0000

    format with black and pylint

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -9,21 +9,19 @@ import argparse
 def get_args():
     """Get the command-line arguments"""
 
-    parser = argparse.ArgumentParser(description='Say Hello')
-    parser.add_argument('-n',
-                        '--name',
-                        metavar='name',
-                        default='World',
-                        help='Name to greet')
+    parser = argparse.ArgumentParser(description="Say Hello")
+    parser.add_argument(
+        "-n", "--name", metavar="name", default="World", help="Name to greet"
+    )
     return parser.parse_args()
 
 
 def main():
     """Say Hello to something"""
-    
+
     args = get_args()
-    print('Hello, ' + args.name + '!')
+    print("Hello, " + args.name + "!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
